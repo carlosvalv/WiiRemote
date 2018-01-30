ef71308d62d13cebf0ce021b1b88c12a956c3a73
chikoritasocute <valverdehx97@gmail.com>
2018-01-30 10:10:02 +0100

    home

--- WiiRemote/FrmMain.vb
+++ WiiRemote/FrmMain.vb
@@ -12,4 +12,28 @@ Public Class FrmMain
             Me.Close()
         End If
     End Sub
+    Private Sub gestio_event_wiimote(ByVal tipus As String, ByVal valor As Object) Handles p_wiimote.event_del_wiimote
+
+        'lbBateria.Text = "nivell de bateria " & Math.Round(p_wiimote.COMANDAMENT.WiimoteState.Battery, 2) & " de 100"
+        tipus = CL_WIIMOTE.WII_EVENT_BOTO
+        If valor = "HOME" Then
+            SendKeys.SendWait("^{ESC}")
+        Else
+
+        End If
+
+        'With p_wiimote.COMANDAMENT.WiimoteState.IRState
+        '    lbIR0.Text = "IR0 : (" & .IRSensors(0).RawPosition.X.ToString & "," & .IRSensors(0).RawPosition.Y.ToString & ") - " & .IRSensors(0).Size
+        '    lbIR1.Text = "IR1 : (" & .IRSensors(1).RawPosition.X.ToString & "," & .IRSensors(1).RawPosition.Y.ToString & ") - " & .IRSensors(1).Size
+        '    lbIR2.Text = "IR2 : (" & .IRSensors(2).RawPosition.X.ToString & "," & .IRSensors(2).RawPosition.Y.ToString & ") - " & .IRSensors(2).Size
+        '    lbIR3.Text = "IR3 : (" & .IRSensors(3).RawPosition.X.ToString & "," & .IRSensors(3).RawPosition.Y.ToString & ") - " & .IRSensors(3).Size
+        '    .Mode = WiimoteLib.IRMode.Extended
+
+        '    lbMidPoint.Text = "(" & .RawMidpoint.X.ToString & "," & .RawMidpoint.Y.ToString & ")"
+        'End With
+
+        'With p_wiimote.COMANDAMENT.WiimoteState.AccelState
+        '    lbAccel.Text = "Accel·leròmetre : X= " & .Values.X.ToString() & ", Y= " & .Values.Y.ToString() & ", Z= " & .Values.Z.ToString()
+        ' End With
+    End Sub
 End Class
